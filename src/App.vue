b676fb0e590c51d61d90bc0c7677860965472e93
lvlongfei <lvlonfei@gmail.com>
2019-02-24 19:51:55 +0800

    生产环境的跨域没解决，目前请求的路径是不对的。

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,50 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
+    <img
+      id="tu1"
+      src="http://5b0988e595225.cdn.sohucs.com/images/20190214/f25953ec264446888d00a42c6a0fde62.jpeg"
+    >
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  display: -webkit-flex;
   color: #2c3e50;
   margin-top: 60px;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+/* #tu {
+  width: 100%;
+  height: 50%;
+  display: flex;
+} */
+
+#tu1 {
+  width: auto;
+  height: auto;
+  max-width: 10%;
+  max-height: 10%;
+  /* text-align: center; */
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="hello">
-    <div id="example-5">
-      <select v-model="platform">
-        
-        <option value="5">pc</option>
-        <option value="2">ps4</option>
-        <option value="1">xbox360</option>
-      </select>
- 
-    </div>
-    <input type="text" v-model="playname" placeholder="请输入你的角色名...">
-    <button @click="search"></button>
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item>
+        <el-select :inline="true" v-model="platform">
+          <el-option value="5" label="PC"></el-option>
+          <el-option value="2" label="PSN"></el-option>
+          <el-option value="1" label="XBOX"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          :inline="true"
+          class="bar"
+          type="text"
+          v-model="playname"
+          placeholder="请输入你的角色名..."
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button :inline="true" class="bar" icon="el-icon-search" @click="search"></el-button>
+      </el-form-item>
+    </el-form>
 
     <p>
       {{ msg }}
-      <br>check out the
-      <a href target="_blank" rel="noopener">vue-cli documentation</a>.
+      <!-- <br>check out the
+      <a href target="_blank" rel="noopener">vue-cli documentation</a>.-->
     </p>
-    <h3>Plugins</h3>
-    <ul>
-      <li>
-        <a href target="_blank" rel="noopener">babel</a>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+
 export default {
+
   name: "HelloWorld",
   data() {
     return {
@@ -46,8 +54,8 @@ export default {
           headers: { "TRN-Api-Key": "b18c95ec-a98e-44d6-9434-72695d83b835" }
         })
         .then(response => (this.msg = response.data))
-        .catch(error => {
-          console.log(error);
+        .catch(() => {
+          // console.log(error);
           this.errored = true;
         })
         .finally(() => (this.loading = false));
@@ -59,6 +67,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+/* .bar {
+  display: flex;
+  flex-flow:row wrap ;
+  display: inline-block;
+  color: #42b983;
+}
+
+.bar el-input{
+  display: inline-block;
+  color: #42b983;
+}
+
+.bar el-button {
+  display: inline-block;
+  color: #42b983;
+} */
+
 h3 {
   margin: 40px 0 0;
 }
